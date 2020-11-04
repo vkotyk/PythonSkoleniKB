f4126727ed8037a933c34bdc2c972d6838eab27b
Václav Kotyk <vaclav_kotyk@kb.cz>
2020-11-04 08:33:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+print ("Hello World")
+
+promenna = "Hello World"
+print (promenna)
+
+pozdrav = 42
+print (pozdrav)
+
+input("Ahoj, jak se jmenuješ? ")
+print ("Těší mě")
+print (f'Aritmetický výraz: {2+2}')
+
+jmeno = input("Ahoj, jak se jmenuješ? \n")
+print (f'Těší\t mě {jmeno}')
+
+cislo = int(input("Zadejte prosím číslo:\n"))
+print (type(cislo))
+print (f'Číslo je {cislo+2}')
+
+cislo = input("Zadejte prosím číslo:\n")
+print (type(cislo))
+print (f'Číslo je {cislo+str(2)}')
+
